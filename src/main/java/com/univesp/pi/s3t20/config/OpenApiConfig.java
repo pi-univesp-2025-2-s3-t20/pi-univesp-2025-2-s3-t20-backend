1a1d17dec26b4b22d494d7c165e7346d07631c13
Richard Cardoso <richard.166@hotmail.com>
2025-10-04 21:00:24 -0300

    env: adicionando url do servidor para openapi docs

--- src/main/java/com/univesp/pi/s3t20/config/OpenApiConfig.java
+++ src/main/java/com/univesp/pi/s3t20/config/OpenApiConfig.java
@@ -1,35 +1,40 @@
 package com.univesp.pi.s3t20.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "PI Univesp 2025 - Sistema de Vendas",
+                version = "1.0.0",
+                description = "API REST para gerenciamento de vendas, clientes, produtos e formas de pagamento",
+                contact = @Contact(
+                        name = "Equipe PI Univesp",
+                        email = "pi@univesp.edu.br",
+                        url = "https://univesp.edu.br"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(url = "${pi-univesp.openapi.server-url:http://localhost:8080}")
+        }
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("PI Univesp 2025 - Sistema de Vendas")
-                        .description("API REST para gerenciamento de vendas, clientes, produtos e formas de pagamento")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Equipe PI Univesp")
-                                .email("pi@univesp.edu.br")
-                                .url("https://univesp.edu.br"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")))
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                ));
+                .components(new Components());
     }
 }
